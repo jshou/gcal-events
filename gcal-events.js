9a5e5ab992604bfb77d4b7df09e9efea9f7627d8
jshou <josh@carbonfive.com>
2019-03-30 17:24:35 -0700

    use moment timezone, bump to 0.1.1

--- gcal-events.js
+++ gcal-events.js
@@ -1,7 +1,7 @@
 import { from } from 'rxjs';
 import { concatMap, map } from 'rxjs/operators';
 import axios from 'axios';
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 const getEvents = (calendarId, calendarSecret, options = {}) => {
   const yesterday = new Date(Date.now() - 864e5); // 864e5 == 86400000 == 24*60*60*1000
